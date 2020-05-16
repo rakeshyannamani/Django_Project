9d8c72be2d1202a0c6726ec6457cfb140dc0b245
Rakesh Yannamani <RAYAI@vestas.com>
2020-05-16 19:56:20 +0530

    init

---
+++ pages/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Page, Company, Programmers, Language
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'update_date')
+    ordering = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Page, PageAdmin)
+
+admin.site.register(Company)
+admin.site.register(Language)
+admin.site.register(Programmers)
---
+++ pages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from . models import Page
+
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
+    # assert False
+    return render(request, 'pages/page.html', context)
+
+# def index(request):
+#     return HttpResponse("<h1>The MySite Homepage</h1>")
+#     return render(request, 'base.html')
+#     return render(request, 'pages/page.html')
+
